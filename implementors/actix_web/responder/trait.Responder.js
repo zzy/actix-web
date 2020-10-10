04f037611fd286a4e494bcd81b1cc23c6d32a3e1
JohnTitor <huyuumi.dev@gmail.com>
2020-10-10 00:29:00 +0000

    Deploying to gh-pages from  @ d765e9099dbda47831a72d3fa149ec03f4e37576 🚀

--- implementors/actix_web/responder/trait.Responder.js
+++ implementors/actix_web/responder/trait.Responder.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["actix_files"] = [{"text":"impl Responder for <a class=\"struct\" href=\"actix_files/struct.NamedFile.html\" title=\"struct actix_files::NamedFile\">NamedFile</a>","synthetic":false,"types":["actix_files::named::NamedFile"]}];
+implementors["actix_files"] = [{"text":"impl Responder for NamedFile","synthetic":false,"types":[]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- implementors/actix_web/service/trait.HttpServiceFactory.js
+++ implementors/actix_web/service/trait.HttpServiceFactory.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["actix_files"] = [{"text":"impl HttpServiceFactory for <a class=\"struct\" href=\"actix_files/struct.Files.html\" title=\"struct actix_files::Files\">Files</a>","synthetic":false,"types":["actix_files::files::Files"]}];
+implementors["actix_files"] = [{"text":"impl HttpServiceFactory for Files","synthetic":false,"types":[]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
