75569d27791b22788788182ec3676c8e4cf6e8bc
robjtede <robjtede@icloud.com>
2021-01-03 23:50:01 +0000

    Deploying to gh-pages from  @ 32de9f8840a39d42f070a1a87b26ed3d32a80fb5 🚀

--- implementors/actix_service/trait.IntoServiceFactory.js
+++ implementors/actix_service/trait.IntoServiceFactory.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["actix_web"] = [{"text":"impl&lt;T&gt; IntoServiceFactory&lt;T&gt; for Resource&lt;T&gt; <span class=\"where fmt-newline\">where<br>&nbsp;&nbsp;&nbsp;&nbsp;T: ServiceFactory&lt;Config = (), Request = ServiceRequest, Response = ServiceResponse, Error = Error, InitError = ()&gt;,&nbsp;</span>","synthetic":false,"types":[]}];
+implementors["actix_web"] = [{"text":"impl&lt;T&gt; IntoServiceFactory&lt;T, ServiceRequest&gt; for Resource&lt;T&gt; <span class=\"where fmt-newline\">where<br>&nbsp;&nbsp;&nbsp;&nbsp;T: ServiceFactory&lt;ServiceRequest, Config = (), Response = ServiceResponse, Error = Error, InitError = ()&gt;,&nbsp;</span>","synthetic":false,"types":[]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
