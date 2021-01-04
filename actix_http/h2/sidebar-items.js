f61b80856a786fce89ecfaf518347c5b4dc61a76
robjtede <robjtede@icloud.com>
2021-01-04 00:55:45 +0000

    Deploying to gh-pages from  @ 21f6c9d7a503726a4e35edf4b38283b8ed8de8e2 🚀

--- actix_http/h2/sidebar-items.js
+++ actix_http/h2/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"struct":[["Dispatcher","Dispatcher for HTTP/2 protocol"],["H2Service","`ServiceFactory` implementation for HTTP2 transport"],["Payload","H2 receive stream"]]});
+initSidebarItems({"struct":[["Dispatcher","Dispatcher for HTTP/2 protocol."],["H2Service","`ServiceFactory` implementation for HTTP/2 transport"],["Payload","HTTP/2 peer stream."]]});
