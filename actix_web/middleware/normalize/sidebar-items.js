f8c7f0049a9ab9dcc437a56bed181bb2656e8909
robjtede <robjtede@icloud.com>
2021-01-05 09:54:51 +0000

    Deploying to gh-pages from  @ 68117543ea0ec020af7a5a7581eb4139595da90c 🚀

--- actix_web/middleware/normalize/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-initSidebarItems({"enum":[["TrailingSlash","To be used when constructing `NormalizePath` to define it's behavior."]],"struct":[["NormalizePath","Middleware to normalize a request's path so that routes can be matched less strictly."]]});
