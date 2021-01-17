05ff62018071df9c71eebbb56de075263b4aedfa
robjtede <robjtede@icloud.com>
2021-01-17 05:22:14 +0000

    Deploying to gh-pages from  @ ee1014844487245efd79fbbe68e388751ae5b2b6 🚀

--- implementors/core/str/traits/trait.FromStr.js
+++ implementors/core/str/traits/trait.FromStr.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["actix_http"] = [{"text":"impl FromStr for CacheDirective","synthetic":false,"types":[]},{"text":"impl FromStr for ContentRangeSpec","synthetic":false,"types":[]}];
+implementors["actix_http"] = [{"text":"impl FromStr for CacheDirective","synthetic":false,"types":[]},{"text":"impl FromStr for ContentEncoding","synthetic":false,"types":[]},{"text":"impl FromStr for ContentRangeSpec","synthetic":false,"types":[]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
