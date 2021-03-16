55ef487d65fa733fb36e9247b915f3c4337a1862
robjtede <robjtede@icloud.com>
2021-03-16 16:35:38 +0000

    Deploying to gh-pages from  @ c8f6d37290e907a79a3acf1a20ad6327ba5bfd65 🚀

--- implementors/tokio/io/async_write/trait.AsyncWrite.js
+++ implementors/tokio/io/async_write/trait.AsyncWrite.js
@@ -1,4 +1,3 @@
 (function() {var implementors = {};
 implementors["actix_http"] = [{"text":"impl AsyncWrite for <a class=\"struct\" href=\"actix_http/test/struct.TestBuffer.html\" title=\"struct actix_http::test::TestBuffer\">TestBuffer</a>","synthetic":false,"types":["actix_http::test::TestBuffer"]},{"text":"impl AsyncWrite for <a class=\"struct\" href=\"actix_http/test/struct.TestSeqBuffer.html\" title=\"struct actix_http::test::TestSeqBuffer\">TestSeqBuffer</a>","synthetic":false,"types":["actix_http::test::TestSeqBuffer"]}];
-implementors["awc"] = [{"text":"impl AsyncWrite for <a class=\"struct\" href=\"awc/struct.BoxedSocket.html\" title=\"struct awc::BoxedSocket\">BoxedSocket</a>","synthetic":false,"types":["awc::connect::BoxedSocket"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
