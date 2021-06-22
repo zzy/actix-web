0d350b289a25293de2ba4f343cb92e2b4d17819e
robjtede <robjtede@icloud.com>
2021-06-22 16:50:38 +0000

    Deploying to gh-pages from  @ b1148fd735fddd03d3717b2a90b697be51fac4d2 🚀

--- implementors/core/hash/trait.Hash.js
+++ implementors/core/hash/trait.Hash.js
@@ -1,3 +1,4 @@
 (function() {var implementors = {};
 implementors["actix_http"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"enum\" href=\"actix_http/enum.Protocol.html\" title=\"enum actix_http::Protocol\">Protocol</a>","synthetic":false,"types":["actix_http::Protocol"]}];
+implementors["actix_web"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"struct\" href=\"actix_web/dev/struct.PeerAddr.html\" title=\"struct actix_web::dev::PeerAddr\">PeerAddr</a>","synthetic":false,"types":["actix_web::info::PeerAddr"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
