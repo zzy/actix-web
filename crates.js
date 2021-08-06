b1c7a182196ea19cdfe2500f485502e931689216
robjtede <robjtede@icloud.com>
2021-08-06 21:45:38 +0000

    Deploying to gh-pages from  @ f6e69919ede4872c1d987b4b932c44580190971c 🚀

--- crates.js
+++ crates.js
@@ -1,1 +1,1 @@
-window.ALL_CRATES = ["actix_files","actix_http","actix_http_test","actix_multipart","actix_test","actix_web","actix_web_actors","actix_web_codegen","awc"];
+window.ALL_CRATES = ["actix_files","actix_http","actix_http_test","actix_multipart","actix_router","actix_test","actix_web","actix_web_actors","actix_web_codegen","awc"];
