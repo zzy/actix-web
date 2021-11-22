38c6cefaeacea74c647cc017ff6be9954bd32f99
robjtede <robjtede@icloud.com>
2021-11-22 18:20:38 +0000

    Deploying to gh-pages from  @ e7987e74293f86bf511d83baca23735df5f3d00c 🚀

--- actix_http_test/sidebar-items.js
+++ actix_http_test/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["test_server","Start test server"],["test_server_with_addr","Start `test server` on an existing address binding."],["unused_addr","Get a localhost socket address with random, unused port."]],"struct":[["TestServer","Test server controller"]]});
+initSidebarItems({"fn":[["test_server","Start test server."],["test_server_with_addr","Start `test server` on an existing address binding."],["unused_addr","Get a localhost socket address with random, unused port."]],"struct":[["TestServer","Test server controller"]]});
