5993a79b1591f04548253bcaafaf98c00f0fcff0
zzy <ouds.cg@gmail.com>
2022-06-07 01:30:31 +0000

    Deploying to gh-pages from  @ 8e76a1c77588c4a8214838c7248e7167b13508b1 🚀

---
+++ implementors/actix_http/header/as_name/trait.AsHeaderName.js
@@ -0,0 +1,5 @@
+(function() {var implementors = {};
+implementors["actix_http"] = [];
+implementors["actix_web"] = [];
+implementors["awc"] = [];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
